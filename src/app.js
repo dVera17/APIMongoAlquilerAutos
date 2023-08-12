0ac62fbc3c35809c3885def30a4c1dcf59a80137
dVera17 <dsvera174@gmail.com>
2023-08-12 12:19:46 -0500

    feat: :sparkles: Se utilizan todos los endpoints creados
    Se utilizan todos los endpoints creados

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 import express from 'express';
 import { config } from "dotenv";
+import routerJWT from './routes/JWT.routes.js';
+import verifyToken from './middlewares/JWT/authToken.js';
+import routerCliente from './routes/cliente.routes.js';
+import routerAutomovil from './routes/automovil.routes.js';
+import routerAlquiler from './routes/alquiler.routes.js';
+import routerReserva from './routes/reserva.routes.js';
+import routerEmpleado from './routes/empleado.routes.js';
+import routerSucursal from './routes/sucursal.routes.js';
+
 config();
 const app = express();
 app.set('port', process.env.PORT_SERVER);
 app.use(express.json());
+
+app.use('/token', routerJWT);
+app.use('/api/cliente', verifyToken, routerCliente);
+app.use('/api/automovil', verifyToken, routerAutomovil);
+app.use('/api/alquiler', verifyToken, routerAlquiler);
+app.use('/api/reserva', verifyToken, routerReserva);
+app.use('/api/empleado', verifyToken, routerEmpleado);
+app.use('/api/sucursal', verifyToken, routerSucursal);
 
 export default app;
